846d7270d796311cc38bb91ff8bd4253d7e02614
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 18:35:14 -0300

    Desconto na compra

--- 13/index.js
+++ 13/index.js
@@ -3,4 +3,13 @@ const tipoDePagamento = "credito";
 
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
-
+if(tipoDePagamento === "credito"){
+    let valor = (valorDoProduto-(valorDoProduto*0.05))/100;
+    console.log(`Valor a ser pago: R$${valor.toFixed(2)}`)
+} else if(tipoDePagamento === "debito"){
+    let valor = (valorDoProduto-(valorDoProduto*0.1))/100;
+    console.log(`Valor a ser pago: R$${valor.toFixed(2)}`)
+} else{
+    let valor = (valorDoProduto-(valorDoProduto*0.03))/100;
+    console.log(`Valor a ser pago: R$${valor.toFixed(2)}`)
+}
