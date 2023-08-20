aed66bd48baaeb276e5873f656358d206920c4c9
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 16:18:53 -0300

    Isenção de impostos

--- 07/index.js
+++ 07/index.js
@@ -2,4 +2,15 @@ const aposentada = false;
 const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
-//seu código aqui
+if(aposentada === true){
+    console.log("ISENTA");
+} else if (portadoraDeDoenca === true){
+    console.log("ISENTA");
+} else if(totalDeRendimentos <= 2855970){
+    console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+}else{
+    console.log("PEGA LEAO");
+}
+
+    
+
