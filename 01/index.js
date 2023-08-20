8b527a8908abde5ebd81a5e4e8aa324a300353a6
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 15:19:43 -0300

    Pedra, papel ou tesoura

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,8 @@
 const jogada1 = "pedra"
 const jogada2 = "tesoura"
 
-//seu código aqui
+ if (jogada1 === "pedra" && jogada2 === "tesoura"){
+        console.log(jogada1);
+    }  else if(jogada2 ==="pedra"&& jogada1 === "tesoura"){
+        console.log(jogada2);
+    }
