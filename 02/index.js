fe31380462e02c40321d6e0f20561791b7e58073
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 15:29:56 -0300

    Par ou impar

--- 02/index.js
+++ 02/index.js
@@ -1,4 +1,10 @@
 const jogada1 = 5;
 const jogada2 = 3;
 
-//seu código aqui
+if(jogada1%2==0 ){
+    console.log(`${jogada1} é par`);
+ if(jogada2%2 == 0){
+    console.log(`${jogada2} é par`);
+}} else{
+    console.log("Nenhum dos números é par");
+}
