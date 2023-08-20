ae4aa3d35e67d29227e9a23df2959615c21a98a3
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 17:58:23 -0300

    Parcela do sucesso compartilhado

--- 11/index.js
+++ 11/index.js
@@ -1,8 +1,15 @@
 //renda mensal do aluno, em centavos.
-const rendaMensalEmCentavos = 300000;
+const rendaMensalEmCentavos = 150000;
 
 // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+if(rendaMensalEmCentavos > 200000 && mesesDecorridos < 60){
+    let valor = (rendaMensalEmCentavos/100);
+    let total = valor *0.18;
+    console.log(`O valor da parcela desse mês é ${total.toFixed(2)}`);
+}else{
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.")
+}
