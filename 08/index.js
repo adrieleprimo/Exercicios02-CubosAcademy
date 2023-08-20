dcfc02c5b0b8abd804c294968be2e3ec343a169b
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 16:34:46 -0300

    Montanha russa

--- 08/index.js
+++ 08/index.js
@@ -3,3 +3,14 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+if(idade < 12 || idade > 65){
+    console.log("ACESSO NEGADO");
+}else if(possuiPatologia === true){
+    console.log("ACESSO NEGADO");
+}else if(altura < 150){
+    console.log("ACESSO NEGADO");
+} else if (idade <= 18 || ehEstudante === true){
+    console.log("Você paga 10 reais");
+} else if (idade > 18 && ehEstudante == false){
+    console.log("Você paga 20 reais");
+}
