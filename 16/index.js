37259d5c6a84c86714704621af1f81494d753e14
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 19:02:06 -0300

    Rematricula do estudante

--- 16/index.js
+++ 16/index.js
@@ -2,4 +2,11 @@
 const idadeDoAluno = 16;
 
 //valor verdadeiro ou falso que informa se possui responsável
-const possuiResponsavel = false;
+const possuiResponsavel = true;
+if(idadeDoAluno<18 && possuiResponsavel === true){
+    console.log("Rematrícula realizada com sucesso");    
+} else if(idadeDoAluno>=18){
+    console.log("Rematrícula realizada com sucesso");    
+} else {
+    console.log("Não é possível fazer a rematrícula"); 
+}
