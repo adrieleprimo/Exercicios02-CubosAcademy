98317acad9610ef4b2b6c421b35892d1c03a0353
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 19:13:46 -0300

    Compras online

--- 17/index.js
+++ 17/index.js
@@ -6,3 +6,9 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+if (valorPago!= valorDoProduto){
+    let parcelasRestantes =Math.abs(300/100-10);
+    let valor = (valorDoProduto/100-valorPago)/parcelasRestantes;
+    console.log(`Restam ${parcelasRestantes} parcelas de R$${valor}`)
+}
