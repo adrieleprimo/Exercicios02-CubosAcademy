3ac996d8476c2ddabed35023e4bd3055f49ae02b
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-20 18:55:16 -0300

    Nome, sobrenome e apelido

--- 15/index.js
+++ 15/index.js
@@ -1,4 +1,17 @@
-const primeiroNome = "Mario";
+const primeiroNome = "";
 const sobrenome = "";
-const apelido = "";
+const apelido = "Luanzinho";
 
+if (apelido === "" && sobrenome ==="" && primeiroNome ===""){
+    console.log("Você é um indigente, sabia?");
+} else if(apelido === "" && sobrenome ==="" && primeiroNome != ""){
+    console.log(`${primeiroNome}`);
+} else if (apelido != "" && sobrenome ==="" && primeiroNome != ""){
+    console.log(`${primeiroNome} ${apelido}`);
+} else if (apelido === "" && sobrenome !="" && primeiroNome != ""){
+    console.log(`${primeiroNome} ${sobrenome}`);
+} else if (apelido != "" && sobrenome ==="" && primeiroNome === ""){
+    console.log(`${apelido}`);
+} else{
+    console.log(`${sobrenome}`);
+}
